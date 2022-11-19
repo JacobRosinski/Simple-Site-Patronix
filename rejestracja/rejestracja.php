f4083f918adcab9aaac9fd051f5547a31815f3d3
BlanKu <jakub.rosinski2k4@gmail.com>
2022-11-19 12:21:27 +0100

    Rejestracja + PHP

--- rejestracja/rejestracja.php
+++ rejestracja/rejestracja.php
@@ -1,15 +1,31 @@
-<?php
-    $mail = $_POST[""];
-    $haslo = $_POST[""];
-    $nazwisko = $_POST[""];
-    $imie = $_POST[""];
-    $adres = $_POST[""];
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Projekt</title>
+        <link rel="stylesheet" href="rejestracja.css">
+    </head>
+    <body>
+        <div id="rejestracja">
+            <?php
+                $mail = $_POST["mail"];
+                $haslo = $_POST["password"];
+                $nazwisko = $_POST["nazwisko"];
+                $imie = $_POST["imie"];
+                $adres = $_POST["adres"];
 
-    $conn = mysqli_connect("localhost","root","","baza");
-    $sql = "INSERT INTO uzytkownicy(mail, haslo) VALUES ('$mail','$haslo'); INSERT INTO dane_uzytkownika(mail, naziwsko, imie, adres) VALUES('$mail',NULL,NULL,NULL);";
+                $conn = mysqli_connect("localhost","root","","baza");
+                $sql = "INSERT INTO uzytkownicy(mail, haslo) VALUES ('$mail','$haslo');";
+                $sql2 = "INSERT INTO dane_uzytkownika(mail, nazwisko, imie, adres) VALUES('$mail','$nazwisko','$imie','$adres');";
+                $conn->query($sql);
+                $conn->query($sql2);
 
-    echo "Dane zostały wysłane";
+                echo "<h1>DANE ZOSTAŁY WYSŁANE</h1>";
 
-    $conn->close()
-
-?>
+                $conn->close()
+            ?>
+        </div>
+    </body>
+</html>
