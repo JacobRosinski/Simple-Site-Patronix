ad1e8f7b481ab31ec734eb695d661cd07f0e83dd
Michał Kuś <118544247+MichalKusKurs@users.noreply.github.com>
2022-11-21 17:11:08 +0100

    nowa strona - beta

--- skrypty.js
+++ skrypty.js
@@ -5,3 +5,11 @@ function dom(){
 function dom2(){
     document.getElementById('ikona').src="img/home.png"
 }
+
+function dom_alt(){
+    document.getElementById('ikona_alt').src="img/home-p.png"
+}
+
+function dom2_alt(){
+    document.getElementById('ikona_alt').src="img/home.png"
+}
